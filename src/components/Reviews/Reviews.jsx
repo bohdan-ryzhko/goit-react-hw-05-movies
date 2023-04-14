5ac39c4865f3e8141abcf652727d01f8643024b4
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-04-14 17:36:50 +0300

    add reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchReviews } from "services/fetchReviews";
+
 
 const Reviews = () => {
+
+	const [reviews, setReviews] = useState([]);
+
+	const { movieId } = useParams();
+
+	useEffect(() => {
+		const controller = new AbortController();
+		
+		fetchReviews(movieId, controller.signal)
+			.then(({ data }) => {
+				setReviews(data.results);
+				console.log(data);
+			}).catch(error => {
+				console.log(error);
+			})
+
+		return () => {
+			controller.abort();
+		}
+	}, [movieId]);
+
 	return (
-		<div>Reviews</div>
+		<>
+			{
+				reviews.length === 0
+					? <p>We don't have ane reviews for this movie</p>
+					: (
+						<ul>
+							{
+								reviews.map(review => <li key={review.id}>
+									<h3>Author: {review.author}</h3>
+									<p>{review.content}</p>
+								</li>)
+							}
+						</ul>
+					)
+			}
+		</>
 	)
 }
 
---
+++ src/services/fetchReviews.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import { API_KEY } from "constants/apiKey";
+
+export const fetchReviews = async (movieId, signal) => {
+	try {
+		return await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews`, {
+			signal,
+			params: {
+				api_key: API_KEY,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
